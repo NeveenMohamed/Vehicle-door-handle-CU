1fa898bc43f753d63fc54c1995fff9d54cde9fb1
Neveen Mohamed Ayman <94166833+NeveenMohamed@users.noreply.github.com>
2023-07-06 20:44:19 +0300

    Delete unwanted files

--- Code/VehicleDoorHandle_ControlUnit/Release/Gpio/Gpio.d
+++
@@ -1,9 +0,0 @@
-Gpio/Gpio.o: ../Gpio/Gpio.c ../Gpio/Gpio.h ../Gpio/Gpio_Private.h \
- C:\Users\Anwar\Desktop\SBME\ 2024\YEAR\ 3\ (2022-2023)\SEMESTER\ 2\Embedded\ Systems\Final\ Project\Code\VehicleDoorHandle_ControlUnit\Lib/Std_Types.h \
- C:\Users\Anwar\Desktop\SBME\ 2024\YEAR\ 3\ (2022-2023)\SEMESTER\ 2\Embedded\ Systems\Final\ Project\Code\VehicleDoorHandle_ControlUnit\Lib/Utils.h \
- C:\Users\Anwar\Desktop\SBME\ 2024\YEAR\ 3\ (2022-2023)\SEMESTER\ 2\Embedded\ Systems\Final\ Project\Code\VehicleDoorHandle_ControlUnit\Lib/Std_Types.h
-../Gpio/Gpio.h:
-../Gpio/Gpio_Private.h:
-C:\Users\Anwar\Desktop\SBME\ 2024\YEAR\ 3\ (2022-2023)\SEMESTER\ 2\Embedded\ Systems\Final\ Project\Code\VehicleDoorHandle_ControlUnit\Lib/Std_Types.h:
-C:\Users\Anwar\Desktop\SBME\ 2024\YEAR\ 3\ (2022-2023)\SEMESTER\ 2\Embedded\ Systems\Final\ Project\Code\VehicleDoorHandle_ControlUnit\Lib/Utils.h:
-C:\Users\Anwar\Desktop\SBME\ 2024\YEAR\ 3\ (2022-2023)\SEMESTER\ 2\Embedded\ Systems\Final\ Project\Code\VehicleDoorHandle_ControlUnit\Lib/Std_Types.h:
